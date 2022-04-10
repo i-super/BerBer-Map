08fe4280367f115506664ac59a7ade7c8ee13fc1
p-ryder <superdeveloper774@gmail.com>
2022-04-10 15:58:23 -0700

    unsubscribe subject

--- src/app/map/map.ts
+++ src/app/map/map.ts
@@ -1,5 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnDestroy, ViewChild } from '@angular/core';
 import { GoogleMap } from '@angular/google-maps';
+import { ReplaySubject, takeUntil } from 'rxjs';
 import { FirebaseService } from '../services/firebase_service';
 
 @Component({
@@ -7,13 +8,20 @@ import { FirebaseService } from '../services/firebase_service';
   templateUrl: 'map.html',
   styleUrls: ['./map.scss'],
 })
-export class MapComponent {
+export class MapComponent implements OnDestroy {
   @ViewChild(GoogleMap) map!: GoogleMap;
 
+  private readonly destroyed = new ReplaySubject<void>(1);
+
   constructor(readonly firebaseService: FirebaseService) {
-    this.firebaseService.authReady.subscribe(() => {
+    this.firebaseService.authReady.pipe(takeUntil(this.destroyed)).subscribe(() => {
       this.firebaseService.fetchSpots();
     });
+  }
+
+  ngOnDestroy() {
+    this.destroyed.next();
+    this.destroyed.complete();
   }
 
   onMarkerClick(marker: any) {
