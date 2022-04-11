3578fa529739737a3fe1a4cb4dd2ca32150bbae2
p-ryder <superdeveloper774@gmail.com>
2022-04-10 19:10:26 -0700

    center to user location

--- src/app/map/map.ts
+++ src/app/map/map.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnDestroy, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import { GoogleMap } from '@angular/google-maps';
 import { ReplaySubject, takeUntil } from 'rxjs';
 import { FirebaseService } from '../services/firebase_service';
@@ -11,8 +11,22 @@ import { mapStyle } from './map_style';
 })
 export class MapComponent implements AfterViewInit, OnDestroy {
   @ViewChild(GoogleMap) map!: GoogleMap;
+  @ViewChild('centerButton', { static: true }) centerButton!: ElementRef;
 
   readonly mapStyle = mapStyle;
+
+  myLocation?: google.maps.LatLngLiteral;
+  readonly myLocationMarkerOptions: google.maps.MarkerOptions = {
+    draggable: false,
+    icon: {
+      path: 'M12 7a5 5 0 1 0 0 10 5 5 0 0 0 0-10zm0-5a10 10 0 1 0 0 20 10 10 0 0 0 0-20zm0 18a8 8 0 1 1 0-16 8 8 0 0 1 0 16z',
+      fillOpacity: 1,
+      fillColor: '#07f',
+      strokeWeight: 0,
+    },
+  };
+
+  private watchPosition?: number;
 
   private readonly destroyed = new ReplaySubject<void>(1);
 
@@ -25,21 +39,36 @@ export class MapComponent implements AfterViewInit, OnDestroy {
   ngAfterViewInit() {
     // Get user's geolocation.
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position: GeolocationPosition) => {
+      this.watchPosition = navigator.geolocation.watchPosition((position: GeolocationPosition) => {
         const pos = {
           lat: position.coords.latitude,
           lng: position.coords.longitude,
         };
+        this.myLocation = pos;
         this.map.panTo(pos);
       });
     } else {
-      // Browser doesn't support Geolocation
+      // Browser doesn't support Geolocation.
     }
+
+    // Add a center button.
+    this.map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(
+      this.centerButton.nativeElement
+    );
   }
 
   ngOnDestroy() {
+    if (this.watchPosition !== undefined) {
+      navigator.geolocation?.clearWatch(this.watchPosition);
+    }
     this.destroyed.next();
     this.destroyed.complete();
+  }
+
+  centerToUserLocation() {
+    if (this.myLocation) {
+      this.map.panTo(this.myLocation);
+    }
   }
 
   onMarkerClick(marker: any) {
